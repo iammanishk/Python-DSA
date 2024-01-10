ac8d5b07e33e06551ad87f17bdca1b807649c124
iammanishk <iammaanisshhh@gmail.com>
2024-01-10 10:47:27 +0530

    File Updated

--- Code/02_Intermediate/01_Conditioinals.py
+++ Code/02_Intermediate/01_Conditioinals.py
@@ -1,2 +1,30 @@
 # ========================================================= CONDITIONAL OPERATORS =========================================================
 
+# ---------------------------------------------------------- IF ELSE ----------------------------------------------------------
+
+
+# Write a program to check whether a number is positive, negative or zero using if else.
+
+# number = int(input("Enter any number! "))
+
+# if number > 0:
+#     print("The number is positive!")
+# elif number == 0:
+#     print("The number is zero!")
+# elif number < 0:
+#     print("The number is negative!")
+# else:
+#     print("Enter a valid number")
+
+
+# Write a program to check that a positive integer is eeven or odd
+    
+number = int(input("Enter any number! "))
+
+if number > 0:
+    if number % 2 == 0:
+        print("The number is even!")
+    else:
+        print("The number is odd!")
+else:
+    print("Enter a valid number")
