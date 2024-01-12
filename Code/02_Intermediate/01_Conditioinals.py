9760dee02122f1d8636c4beef265ca54d070ee46
iammanishk <iammaanisshhh@gmail.com>
2024-01-12 17:32:26 +0530

    File Updated

--- Code/02_Intermediate/01_Conditioinals.py
+++ Code/02_Intermediate/01_Conditioinals.py
@@ -5,6 +5,8 @@
 
 # Write a program to check whether a number is positive, negative or zero using if else.
 
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> CODE <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
 # number = int(input("Enter any number! "))
 
 # if number > 0:
@@ -17,14 +19,40 @@
 #     print("Enter a valid number")
 
 
-# Write a program to check that a positive integer is eeven or odd
-    
-number = int(input("Enter any number! "))
+# Write a program to check that a positive integer is even or odd
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> CODE <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-if number > 0:
-    if number % 2 == 0:
-        print("The number is even!")
-    else:
-        print("The number is odd!")
-else:
-    print("Enter a valid number")
+    
+# number = int(input("Enter any number! "))
+
+# if number > 0:
+#     if number % 2 == 0:
+#         print("The number is even!")
+#     else:
+#         print("The number is odd!")
+# else:
+#     print("Enter a valid number")
+
+# Write a program that will print the profit or loss of the product based on the cost price and sellling price
+  
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> CODE <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+# cost_Price = int(input("Enter the cost price of the product \n"))
+# sellling_Price = int(input("Enter the selling price of the product \n"))
+
+# if sellling_Price > cost_Price:
+#     profit = sellling_Price - cost_Price
+#     print("You have the profit of ", profit , "Rupees")
+# elif cost_Price > sellling_Price:
+#     loss = cost_Price - sellling_Price
+#     print("You are in the loss of ", loss, "Rupees")
+# elif cost_Price == sellling_Price:
+#     print("\nYou are not in either Profit or Loss")
+# else:
+#     ("Enter valid data")
+
+
+
+# Write a program that will print the profit or loss of the product based on the cost price and sellling price
+  
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> CODE <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
