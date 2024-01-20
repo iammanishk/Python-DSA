15eb03114aa878e24942b7d25d369192cb310e00
iammanishk <iammaanisshhh@gmail.com>
2024-01-20 20:39:45 +0530

    File Updated

--- Code/02_Intermediate/Python_Collection/List.py
+++ Code/02_Intermediate/Python_Collection/List.py
@@ -6,11 +6,72 @@
 my_list = ["Manish", "Yadhuvanshi", "Manish", 21, 854327]
 
 # Access Items
-print(my_list) # this will print whole list
-print(len(my_list))
-print(my_list[3]) # this will print 854327
-print(my_list[0]) # this will print Manish
+# print(my_list) 
+# print("This is the length of the list: ",len(my_list))
+# print(my_list[3]) 
+# print(my_list[0]) 
+# print(my_list[0:2])
 
 # Negative Indexing
-print(my_list[-1]) # this will print 854327
-print(my_list[-3]) # this will print Y5432adhuvanshi
+# print(my_list[-1]) 
+# print(my_list[-3]) 
+
+# Checking if the element is present in the list:
+
+# if "Manish" in my_list:
+#     print("Yes")
+# else:
+#     print("No")
+
+# Adding element in a list
+    # There are three ways to add elements in a list:
+        # 1. append()
+        # 2. insert()
+        # 3. extend()
+
+my_list1 = [10,12,14,16,18,20]
+
+# append()
+my_list1.append(22)
+# print(my_list1)
+
+# insert()
+my_list1.insert(1,"Number")
+# print(my_list1)
+
+# extend()
+list3 = [30,31,32,33,34,35]
+my_list1.extend(list3)
+# print(my_list1)
+
+
+# Removing elements from a list
+    # There are three ways to remove elements from a list:
+        # 1. remove()
+        # 2. pop()
+        # 3. del
+
+
+# remove() 
+list3.remove(35)
+print(list3)
+
+# pop()
+list3.pop(2)
+print(list3)
+
+# del
+
+list4 = [1,2,3,4,5]
+list5 = [6,7,8,9,10]
+
+list4.extend(list5)
+print("This is extended list:")
+print(list4)
+
+# removing list using del function
+
+del list4[2:5]
+print("This is the list after deleting some elements:")
+print(list4)
+
--- Code/02_Intermediate/Python_Collection/NOTES.md
+++ Code/02_Intermediate/Python_Collection/NOTES.md
@@ -15,5 +15,72 @@
 example of list :
 
 list = ["Manish", "kumar", 21, 854327]
+print(list)
 ```
 
+## *There are three ways to add elements in a list*
+1. append()
+2. insert()
+3. extend()
+
+- ### Append add item to the end in a list
+```
+example of append():
+
+list.append("Bihar")
+print(list)
+```
+
+
+- ### Insert add element at any place by using index
+```
+example of insert():
+
+list.insert(2,"Male")
+print(list)
+```
+
+- ### Extend is used to add another list in the current list
+```
+example of extend():
+
+new_list = ["BCA", "MMDU"]
+list.extend(new_list)
+print(list)
+```
+
+
+## *There are three ways to remove an element from a list*
+1. remove()
+2. pop()
+3. del()
+
+```
+new_list1 = [1,2,3,4,5]
+new_list2 = [6,7,8,9,10]
+```
+- ### remove() deletes the element by giving exactely value
+```
+example of remove():
+
+new_list2.remove(10)
+print(new_list2)
+```
+
+- ### pop() deletes an element by using index of the element
+```
+example of pop():
+
+new_list2.pop(2)
+print(new_list2)
+```
+
+- ## del() delets the more than one element in list
+
+```
+example of del();
+
+del new_list1[1:3]
+print(new_list1)
+# ye first index se second index (3-1) tak delet krta hai
+```
