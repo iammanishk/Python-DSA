f81f3febeca6b140b1ee778114fb31baf20f38f4
iammanishk <iammaanisshhh@gmail.com>
2024-02-01 02:22:38 +0530

    File Modified

--- Code/02_Intermediate/Python_Collection/04_Dictionary.py
+++ Code/02_Intermediate/Python_Collection/04_Dictionary.py
@@ -23,13 +23,13 @@ student = {
 # Accessing the dictionary:
 # You can access the items of a dictionary by referring to its key name, inside square brackets []. If the item is not found, it will raise a KeyError.
 
-# print("Name of student: ", student['name'])
-# print("Age of student: ", student['grade'])
+print("Name of student: ", student['name'])
+print("Age of student: ", student['grade'])
 
 # If you try to access a key that doesn’t exist in the dictionary, Python will raise an error. 
 # To avoid this we use get() method which returns 'None' if the key does not exist in the dictionary.
 
-# print("Phone number of student: ", student.get('phone'))
+print("Phone number of student: ", student.get('phone'))
 
 
 # Modifying Values:
@@ -37,7 +37,7 @@ student = {
 # You can change the value of a specific item by referring to its key name.
 
 student['age'] = 20
-# print(student)
+print(student)
 
 # Adding New Key-Value Pairs:
 
@@ -45,7 +45,7 @@ student['age'] = 20
 
 student['email'] = "iammaanisshhh@gmail.com"
 student['grade'] = "A+"
-# print(student)
+print(student)
 
 # You can also use update() method to add new key-value pairs to the dictionary.
 
@@ -56,48 +56,47 @@ address = {
 }
 
 student.update(address)
-# print(student)
+print(student)
 
 # Removing Key-Value Pairs:
 
 # Removing an item from the dictionary is done by using the pop() method. This method removes the item with the specified key name.
-
 # student.pop('grade') # This removes the whole pair (key & Value).
-# print(student)
+print(student)
 
 # The popitem() method removes the last inserted item (in versions before 3.7, a random item is removed instead).
 
-# student.popitem()
-# print(student)
+student.popitem()
+print(student)
 
 # You can remove the key-value pair using del keyword also.
-# del student['email']
-# print(student)
+del student['email']
+print(student)
 
 # You can delet all the key-value pairs using clear() method.
 
-# student.clear()
-# print(student)
+student.clear()
+print(student)
 
 # Checking if the item is present in the dictionary or not:
 
 # To determine if a specified key is present in a dictionary use the 'in' keyword.
 
-# if 'age' in student:
-#     print("Yes, 'age' is one of the keys in the student dictionary")
-# else:
-#     print("No, 'age' is not any of the keys in the student dictionary")
+if 'age' in student:
+    print("Yes, 'age' is one of the keys in the student dictionary")
+else:
+    print("No, 'age' is not any of the keys in the student dictionary")
 
 # Printing the elements of the dictionary:
 
 # You can print all the keys of the dictionary using keys() method.
-# print(student.keys())
+print(student.keys())
 
 # You can print all the values of the dictionary using values() method.
-# print(student.values())
+print(student.values())
 
 # You can print all the key-value pairs of the dictionary using items() method.
-# print(student.items())
+print(student.items())
 
 # Looping through a dictionary:
 for x in student:
