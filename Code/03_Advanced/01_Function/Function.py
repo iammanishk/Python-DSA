91264c1aa066f619fb7e9d3f1b9c4924c94c51d0
iammanishk <iammaanisshhh@gmail.com>
2024-02-02 01:13:18 +0530

    Function is Added

---
+++ Code/03_Advanced/01_Function/Function.py
@@ -0,0 +1,8 @@
+# ========================================================= FUNCTION =========================================================
+
+# In python, a function is a reusable code that perform a specific task.
+# A function is a block of code which only runs when it is called.
+# Function helps in organizing code, making it more readable,reusable, modular and maintainable.   
+
+# ========================================================= SYNTAX =========================================================
+
---
+++ Code/03_Advanced/01_Function/NOTES.md
@@ -0,0 +1,6 @@
+# FUNCTION
+
+- A function is a block of reusable code that perform a specific task.
+- Function helps in organizing the code,by making it more readable, modular and easier to maintain.
+
+## Defining Function:
